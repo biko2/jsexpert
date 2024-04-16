64499aff5145ade6123c6f44fc6333bd7387242a
Cilveti <inigo.cilveti@biko2.com>
2024-04-16 03:03:10 +0200

    simplify problem

--- ejercicios/0_introduccion/1_variables.test.js
+++ ejercicios/0_introduccion/1_variables.test.js
@@ -1,23 +1,15 @@
 
 if(typeof valor === "undefined") {
   //--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
+  
   var valor = 1;
-  console.log(valor);
+  
   //--------- ⬆️ Modifica el código de arriba ⬆️ -----------//
-
-  if(typeof valor === "undefined") {
-    throw new Error("valor no está definido 🤟😔");
-  }
+  console.log(valor);
 }
 
-const valorExiste = typeof valor !== "undefined";
-
-if(!valorExiste) {
-  var valor = 2;
-}
-
-it('mensaje debe ser 2', () => {
-  expect(valor).toBe(2);
+it('valor no debería existir aquí', () => {
+  expect(typeof valor).toBe('undefined');
 });
 
 
