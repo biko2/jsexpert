ca8cb548cddb8726d7546a4354e0d59380668fa8
Cilveti <inigo.cilveti@biko2.com>
2024-04-27 02:40:17 +0200

    reframe

---
+++ ejercicios/0_variables/variables_7_.test.js
@@ -0,0 +1,13 @@
+
+const textoVulgar = "Hostia, no jodas";
+
+const textoCorregido = textoVulgar
+//--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
+
+
+//--------- ⬆️ Modifica el código de arriba ⬆️ -----------//
+
+it("el textoVulgar debe ser 'Ostras, no fastidies'", () => {
+  expect(textoCorregido).toBe("Ostras, no fastidies");
+});
+
