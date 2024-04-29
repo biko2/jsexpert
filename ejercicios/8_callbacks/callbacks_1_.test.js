8b828b307205dd2b11ae5d3cfc6dc6ea9ead6c97
Teodor Mivovan <teodor.mivovan@biko2.com>
2024-04-29 22:42:14 +0200

    mejora test callback

--- ejercicios/8_callbacks/callbacks_1_.test.js
+++ ejercicios/8_callbacks/callbacks_1_.test.js
@@ -3,14 +3,15 @@ const calculadora = {
     //--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
     //--------- ⬆️ Modifica el código de arriba ⬆️ -----------//
   },
-
-  pinto: function (numero, callback) {
+};
+const formateadorDeTexto = {
+  formateo: function (numero, callback) {
     //--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
     //--------- ⬆️ Modifica el código de arriba ⬆️ -----------//
   },
 };
 
-const multiplicoYPintoPorConsola = (a, b) => {
+const multiplicoYPintoPorConsolaTextoFormateado = (a, b) => {
   calculadora.multiplico(
     a
     //--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
@@ -23,10 +24,10 @@ describe("Callbacks", () => {
   it("deberíamos multiplicar y pintar por consola el resultado", () => {
     vi.spyOn(console, "log");
     vi.spyOn(calculadora, "multiplico");
-    vi.spyOn(calculadora, "pinto");
-    multiplicoYPintoPorConsola(2, 3);
+    vi.spyOn(formateadorDeTexto, "formateo");
+    multiplicoYPintoPorConsolaTextoFormateado(2, 3);
     expect(calculadora.multiplico).toHaveBeenCalled();
-    expect(calculadora.pinto).toHaveBeenCalled(6);
+    expect(formateadorDeTexto.formateo).toHaveBeenCalled();
     expect(console.log).toHaveBeenCalledWith("El resultado es 6");
   });
 });
