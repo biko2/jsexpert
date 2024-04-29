22272121b77a736ef0de267cfd87401fa06b8c3b
Teodor Mivovan <teodor.mivovan@biko2.com>
2024-04-29 23:30:12 +0200

    modificar nombre

--- ejercicios/9_array_functions/map_0_.test.js
+++ ejercicios/9_array_functions/map_0_.test.js
@@ -4,8 +4,8 @@ const getResto = (divisor, ...numeros) => {
 };
 
 describe("Map", () => {
-  it("devuelve los módulos de los números proporcionados", () => {
-    const modulos = getResto(2, 12, 46, 56, 76, 88, 105);
-    expect(modulos).toEqual([0, 0, 0, 0, 0, 1]);
+  it("devuelve el resto de los números proporcionados", () => {
+    const restos = getResto(2, 12, 46, 56, 76, 88, 105);
+    expect(restos).toEqual([0, 0, 0, 0, 0, 1]);
   });
 });
