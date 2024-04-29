2147f4915d327ba3862a72fee0617178527a7b99
Teodor Mivovan <teodor.mivovan@biko2.com>
2024-04-30 00:32:32 +0200

    naming

---
+++ ejercicios/9_array_functions/array_functions_0_.test.js
@@ -0,0 +1,11 @@
+const getResto = (divisor, ...numeros) => {
+  //--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
+  //--------- ⬆️ Modifica el código de arriba ⬆️ -----------//
+}
+
+describe('Array functions', () => {
+  it('devuelve el resto de los números proporcionados', () => {
+    const restos = getResto(2, 12, 46, 56, 76, 88, 105)
+    expect(restos).toEqual([0, 0, 0, 0, 0, 1])
+  })
+})
--- ejercicios/9_array_functions/array_functions_3_.test.js
+++ ejercicios/9_array_functions/array_functions_3_.test.js
@@ -5,51 +5,51 @@
 // Necesitamos una función getFactura que procesa todas las personas que han asistido al evento y devuelve un array con la consumición, la cantidad y el total a pagar por cada persona.
 
 const diana = {
-  nombre: "Diana",
+  nombre: 'Diana',
   edad: 25,
   rondas: 3,
-};
+}
 
 const tom = {
-  nombre: "Tom",
+  nombre: 'Tom',
   edad: 15,
   rondas: 2,
-};
+}
 
 const jhonny = {
-  nombre: "Jhonny",
+  nombre: 'Jhonny',
   edad: 49,
   rondas: 1,
-};
+}
 
 const getFactura = (personas) => {
   //--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
   //--------- ⬆️ Modifica el código de arriba ⬆️ -----------//
-};
+}
 
-describe("Map", () => {
-  it("en un evento se sirve una consumición a cada persona según la edad", () => {
-    const personas = [diana, tom, jhonny];
-    const cuentas = getFactura(personas);
+describe('Array functions', () => {
+  it('en un evento se sirve una consumición a cada persona según la edad', () => {
+    const personas = [diana, tom, jhonny]
+    const cuentas = getFactura(personas)
     expect(cuentas).toEqual([
       {
-        nombre: "Diana",
-        consumicion: "cerveza",
+        nombre: 'Diana',
+        consumicion: 'cerveza',
         cantidad: 3,
         total: 6,
       },
       {
-        nombre: "Tom",
-        consumicion: "zumo",
+        nombre: 'Tom',
+        consumicion: 'zumo',
         cantidad: 2,
         total: 2,
       },
       {
-        nombre: "Jhonny",
-        consumicion: "whisky",
+        nombre: 'Jhonny',
+        consumicion: 'whisky',
         cantidad: 1,
         total: 3,
       },
-    ]);
-  });
-});
+    ])
+  })
+})
